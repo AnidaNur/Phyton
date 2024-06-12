6092b566a576c75fbf625e1a854a3343d786efde
Cchocolas <148960760+AnidaNur@users.noreply.github.com>
2024-06-12 20:52:18 +0700

    Add files via upload

---
+++ Sunsun.py
@@ -0,0 +1,13 @@
+my_list = []
+my_list = [1,2,3,4,5]
+my_list = ["Virus sunsun"]
+print(my_list)
+
+Happy = ['Its  Sunoo']
+print (Happy)
+
+Sunsun = ['Happy virus geng']
+print (Sunsun)
+
+heh = ['Sunoo,jungwon,seunghon']
+print (heh)
